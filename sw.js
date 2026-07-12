b1dbc7bd6d89b6a9976d525c04f5bba140bcd747
maxprescott02-source <maxprescott02@gmail.com>
2026-07-12 18:32:15 +1000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,8 @@
 /* EzPlate service worker */
-const CACHE = 'ezplate-v27';
+const CACHE = 'ezplate-v28';
 const ASSETS = [
   './', './index.html', './css/style.css', './js/app.js',
+  './css/style.css?v=28', './js/app.js?v=28',
   './manifest.json', './icons/icon-192.png', './icons/icon-512.png'
 ];
 self.addEventListener('install', function(e){
